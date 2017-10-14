0ec480f075fe68e2e7d88b92b4a232664aef84ea
zhangyunkai <yxlc785@163.com>
2017-10-14 23:34:12 +0800

    deal bug

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
-import sys
 import pygame
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
 	""" 初始化游戏并创建一个屏幕对象 """
@@ -9,16 +10,23 @@ def run_game():
 	screen = pygame.display.set_mode((ai_setting.screen_width,ai_setting.screen_height))
 	pygame.display.set_caption('Alien Invasion')
 	
+    # 创建一艘飞船
+	ship = Ship(screen)
 
 	while True:
 		# 循环监听鼠标和键盘事件
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
+		# for event in pygame.event.get():
+		# 	if event.type == pygame.QUIT:
+		# 		sys.exit()
+		gf.check_events(ship)
 
-		screen.fill(ai_setting.bg_color)
-		# 让绘制的屏幕可见
-		pygame.display.flip()
+		ship.moving()
+		# screen.fill(ai_setting.bg_color)
+        # 绘制飞船位置
+		# ship.blitme()
+		# # 让绘制的屏幕可见
+		# pygame.display.flip()
+		gf.update_screen(ai_setting,screen,ship)
 
 
 
--- ship.py
+++ ship.py
@@ -15,8 +15,28 @@ class Ship():
 		self.rect.centerx = self.screen_rect.centerx   # 飞船中心x坐标是整个屏幕的中心的x坐标
 		self.rect.bottom = self.screen_rect.bottom     #  飞船下边缘和整个屏幕下边缘对齐
 
+		# 初始化默认不移动
+		self.moving_right = False  
+		self.moving_left = False
 
-	def blitme():
+
+	def blitme(self):
 		""" 在指定位置绘制飞船  """
 		self.screen.blit(self.image,self.rect)
 
+	# 检查飞机是否已经到达屏幕边缘
+	def check_edge(self):
+	 	if self.rect.left == self.screen_rect.left:
+	 		self.moving_left = False
+ 		if self.rect.right == self.screen_rect.right:
+ 			self.moving_right = False
+
+    # 飞机移动方法
+	def moving(self):
+		self.check_edge()
+		if self.moving_right == True:
+			self.rect.centerx += 1
+		if self.moving_left == True:
+			self.rect.centerx -= 1
+
+   
